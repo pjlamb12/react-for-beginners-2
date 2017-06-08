636d651419c12a02d12ce5360c21978297e5baaa
Preston Lamb <prestonlamb@Prestons-MacBook-Pro.local>
2017-06-08 12:24:33 -0600

    Added Firebase storage to the app on a per store basis

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDUrR4wTPFiRo70b5kCGKd5ixTZBxpLMDQ",
+	authDomain: "catch-of-the-day-pjlamb12.firebaseapp.com",
+	databaseURL: "https://catch-of-the-day-pjlamb12.firebaseio.com"
+});
+
+export default base;
